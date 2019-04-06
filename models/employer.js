965a2cbaa3bfcc27fed49ef3dac0b14751eb7659
Leah Weston <westonl91@yahoo.com>
2019-04-06 12:34:17 -0700

    lint fixes

--- models/employer.js
+++ models/employer.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-    var Employer = sequelize.define("Employer", {
-      email: DataTypes.STRING,
-      password: DataTypes.STRING
-    });
-    return Employer;
-  };
+  var Employer = sequelize.define("Employer", {
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  });
+  return Employer;
+};
