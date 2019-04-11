c23aa7dd1ededc8b676a28d907f89e6035d71a56
Leah Weston <westonl91@yahoo.com>
2019-04-10 20:01:29 -0700

    fixed routes, I think

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,51 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get candidate data for profile page
+  app.get("/api/can/:id", function(req, res) {
+    db.Candidate.findOne({ where: { id: req.params.id } }).then(function(
+      results
+    ) {
+      res.json(results);
+      console.log(results);
+    });
+  });
+
+  //get employer data for profile page
+  app.get("/api/emp/:id", function(req, res) {
+    db.Employer.findOne({ where: { id: req.params.id } }).then(function(
+      results
+    ) {
+      res.json(results);
+      console.log(results);
+    });
+  });
+
+  // Create a new candidate
+  app.post("/api/candidate", function(req, res) {
+    db.Candidate.create(req.body).then(function(dbCan) {
+      res.json(dbCan);
+    });
+  });
+
+  // Create a new employer
+  app.post("/api/employer", function(req, res) {
+    db.Candidate.create(req.body).then(function(dbEmp) {
+      res.json(dbEmp);
+    });
+  });
+
+  // get all openings for candidate page
+  app.get("/api/opening", function(req, res) {
+    db.Opening.findAll().then(function(dbOpens) {
+      res.json(dbOpens);
+    });
+  });
+
+  // get all candidates for employer page
+  app.get("/api/cans", function(req, res) {
+    db.Candidate.findAll().then(function(dbCans) {
+      res.json(dbCans);
     });
   });
 
@@ -24,3 +65,22 @@ module.exports = function(app) {
     });
   });
 };
+
+// // CANDIDATES POST REQUEST
+// app.post("/api/new-candidate", function(req, res) {
+//   var data = req.body;
+//   console.log(data);
+//   // db.
+// });
+// // EMPOLYERS POST REQUEST
+// app.post("/api/new-employer", function(req, res) {
+//   var data = req.body;
+//   console.log(data);
+//   // db.
+// });
+// // OPENINGS POST REQUEST
+// app.post("/api/new-opening", function(req, res) {
+//   var data = req.body;
+//   console.log(data);
+//   // db.
+// });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,29 +1,24 @@
-var db = require("../models");
+//var db = require("../models");
+var path = require("path");
 
 module.exports = function(app) {
-  // Load index page
+  // Load login/home page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // Load candidate profile page
+  app.get("/can/:id", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  // Load employer profile page with their info and employer info
+  app.get("/emp/:id", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/manager.html"));
   });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
-    res.render("404");
+    res.sendFile(path.join(__dirname, "../public/404.html"));
   });
 };
